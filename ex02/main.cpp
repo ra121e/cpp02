1a886804b321cda44ed595b7789f243015ddb477
root <sushi.honda@gmail.com>
2025-05-03 21:28:27 +0800

    ex02) copy paste overflow handling from ex01

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 19:49:58 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/03 19:07:16 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/03 21:27:57 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,13 @@ int	main(void)
 	std::cout << "m / n: " << std::fixed << (m / n) << std::endl;
 
 	Fixed e(0.1f);
-	Fixed f(2.0f);
-	Fixed g(8388607);
+	Fixed f(2.1f);
+	Fixed g(8388608);
 	std::cout << "e / f: " << std::fixed << (e / f) << std::endl;
 	std::cout << "l / f: " << std::fixed << (l / f) << std::endl;
 	std::cout << "l / e: " << std::fixed << (l / e) << std::endl;
 	std::cout << "g / e: " << std::fixed << (g / e) << std::endl;
+	std::cout << "g / f: " << std::fixed << (g / f) << std::endl;
 
 
 //	Fixed const	b(Fixed(5.05f) * Fixed(2));
