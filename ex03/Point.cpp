2c5231d477f6e79335b12eacad91cffe9659b60b
root <sushi.honda@gmail.com>
2025-05-07 09:49:49 +0800

    ex03) just record... wrong way
      Spent a lot time to understand BSP, adn to find the implementation by
      BSP tree, recursive, vector to check front or back, traverse....
    
      Because the description says BSP BSP BSP!!!!!!
    
      but too much. too many things to do. and
      turn in file is only bsp.cpp....
      x and y values in cordinate Point is const, which means no way to copy, to store in tmporary instance...
    
      I will quit. no not quit. suspend... I will be back..

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:54:57 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/06 22:48:15 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/07 09:49:25 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@ Point::Point() :
 	y(0)
 {
 }
+
+Point::Point(float x, float y) :
+	x(x),
+	y(y)
+{
+
+}
+
+Point::Point(Fixed x, Fixed y) :
+	x(x),
+	y(y)
+{}
 
 Point::Point(Point &other) :
 	x(other.x),
@@ -37,3 +49,27 @@ Fixed	Point::getY(void)
 {
 	return (this->y);
 }
+
+Point	Point::vec_sub(Point start, Point end)
+{
+	Point	tmp(end.x - start.x, end.y - start.y);
+
+	return (tmp);
+}
+
+Point	Point::vec_normal(Point a)
+{
+	Point	tmp(a.y * -1, a.x);
+
+	return (tmp);
+}
+
+Fixed	Point::dot(Point a, Point b)
+{
+	return (a.x * b.x + a.y + b.y);
+}
+
+//Point	Point::vec_cross(Point a, Point b);
+//{
+//
+//}
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:39:54 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/06 22:46:12 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/07 09:43:53 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,18 @@ class Point
 {
 public:
 	Point();
+	Point(float x, float y);
+	Point(Fixed x, Fixed y);
 	Point(Point &other);
 	~Point();
 
 	Fixed	getX(void);
 	Fixed	getY(void);
+
+	static Point	vec_sub(Point start, Point end);
+	static Point	vec_normal(Point a);
+	static Fixed	dot(Point a, Point b);
+//	static Point	vec_cross(Point a, Point b);
 
 private:
 	Point	&operator=(const Point &other);
