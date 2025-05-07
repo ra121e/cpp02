57851a8b70189377ea5a5935a56e33976584ff9a
root <sushi.honda@gmail.com>
2025-05-07 10:16:32 +0800

    ex03) restart: very very preparation

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:54:57 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/07 09:49:25 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/07 10:15:52 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,9 @@
 Point::Point() :
 	x(0),
 	y(0)
-{
-}
+{}
 
-Point::Point(float x, float y) :
-	x(x),
-	y(y)
-{
-
-}
-
-Point::Point(Fixed x, Fixed y) :
+Point::Point(const float x, const float y) :
 	x(x),
 	y(y)
 {}
@@ -33,43 +25,17 @@ Point::Point(Fixed x, Fixed y) :
 Point::Point(Point &other) :
 	x(other.x),
 	y(other.y)
-{
-}
+{}
 
 Point::~Point()
-{
-}
+{}
 
-Fixed	Point::getX(void)
+Fixed	Point::getX(void) const
 {
 	return (this->x);
 }
 
-Fixed	Point::getY(void)
+Fixed	Point::getY(void) const
 {
 	return (this->y);
 }
-
-Point	Point::vec_sub(Point start, Point end)
-{
-	Point	tmp(end.x - start.x, end.y - start.y);
-
-	return (tmp);
-}
-
-Point	Point::vec_normal(Point a)
-{
-	Point	tmp(a.y * -1, a.x);
-
-	return (tmp);
-}
-
-Fixed	Point::dot(Point a, Point b)
-{
-	return (a.x * b.x + a.y + b.y);
-}
-
-//Point	Point::vec_cross(Point a, Point b);
-//{
-//
-//}
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:39:54 by athonda           #+#    #+#             */
-/*   Updated: 2025/05/07 09:43:53 by athonda          ###   ########.fr       */
+/*   Updated: 2025/05/07 10:16:17 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,12 @@ class Point
 {
 public:
 	Point();
-	Point(float x, float y);
-	Point(Fixed x, Fixed y);
+	Point(const float x, const float y);
 	Point(Point &other);
 	~Point();
 
-	Fixed	getX(void);
-	Fixed	getY(void);
-
-	static Point	vec_sub(Point start, Point end);
-	static Point	vec_normal(Point a);
-	static Fixed	dot(Point a, Point b);
-//	static Point	vec_cross(Point a, Point b);
+	Fixed	getX(void) const;
+	Fixed	getY(void) const;
 
 private:
 	Point	&operator=(const Point &other);
